81ab6d7abc32fe0cc6cccd58de3dbbdebc6c2595
Eugene Kaler <mr.blinger@gmail.com>
2024-09-17 22:58:39 +0300

    wip

---
+++ Web.DTOs/ArticleDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Web.DTOs
+{
+    public class ArticleDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public string? Text { get; set; }
+        public string? OriginalUrl { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public double? Rate { get; set; }
+        public Guid SourceId { get; set; }
+        public string SourceName { get; set; }
+    }
+}
---
+++ Web.DTOs/UserTokenDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Web.DTOs
+{
+    public class UserTokenDto
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string RoleName { get; set; }
+        public Guid RefreshToken { get; set; }
+    }
+}
