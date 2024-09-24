c3492047ccc7ebb79ed07b849f9ca386c4edf3e9
Eugene Kaler <mr.blinger@gmail.com>
2024-09-24 18:33:25 +0300

    wip

--- WebAPI/Controllers/ArticleController.cs
+++ WebAPI/Controllers/ArticleController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Web.Data;
+using Web.Data.Entities;
 using Web.Mapper;
 using Web.Services.Abstractions;
 
@@ -9,10 +12,12 @@ namespace Web.API.Controllers
     [ApiController]
     public class ArticleController : ControllerBase
     {
+        private readonly ApplicationContext _context;
         private readonly IArticleService _articleService;
 
-        public ArticleController(IArticleService articleService)
+        public ArticleController(ApplicationContext context, IArticleService articleService)
         {
+            _context = context;
             _articleService = articleService;
         }
 
@@ -33,6 +38,55 @@ namespace Web.API.Controllers
 
             var articleDto = ApplicationMapper.ArticleToArticleDto(article);
             return Ok(articleDto);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutArticle(Guid id, Article article)
+        {
+            if (id != article.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(article).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ArticleExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteArticle(Guid id)
+        {
+            var article = await _context.Articles.FindAsync(id);
+            if (article == null)
+            {
+                return NotFound();
+            }
+
+            _context.Articles.Remove(article);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ArticleExists(Guid id)
+        {
+            return _context.Articles.Any(e => e.Id == id);
         }
     }
 }
